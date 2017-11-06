66cf84fbf4326d54ea6d8bac06a0074926742bc0
Ben Cromwell <git@cromwell.dev>
2017-11-06 10:02:22 +0000

    'Extends' option allows for inheriting properties
    The YAML must also be processed in the order it was defined. The SO
    linked answer and code maintains the ordering.

--- sshush/sshush.py
+++ sshush/sshush.py
@@ -1,10 +1,28 @@
 import yaml
+from collections import OrderedDict
+
+
+# https://stackoverflow.com/a/21912744/149998
+def ordered_load(stream, loader=yaml.Loader, object_pairs_hook=OrderedDict):
+    class OrderedLoader(loader):
+        pass
+
+    def construct_mapping(loader, node):
+        loader.flatten_mapping(node)
+        return object_pairs_hook(loader.construct_pairs(node))
+
+    OrderedLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        construct_mapping
+    )
+
+    return yaml.load(stream, OrderedLoader)
 
 
 def read_file(file):
     with open(file, 'r') as stream:
         # try:
-            return yaml.load(stream)
+            return ordered_load(stream, yaml.SafeLoader)
         # except yaml.YAMLError as e:
         #     print(e)
 
@@ -15,21 +33,37 @@ def process_yaml(ssh_config_yaml):
     defaults = extract_section('default', ssh_config_yaml, {})
     global_values = extract_section('global', ssh_config_yaml)
 
+    configs = {}
+
     for identifier, item in ssh_config_yaml.items():
         output.append('# {}'.format(identifier))
         hosts = item['Hosts']
-        del item['Hosts']
 
-        settings = {**defaults, **item}
+        if 'Config' not in item:
+            item['Config'] = {}
+
+        if 'Extends' in item and item['Extends'] in configs:
+            settings = {**defaults, **configs[item['Extends']], **item['Config']}
+        else:
+            settings = {**defaults, **item['Config']}
+
+        configs[identifier] = settings
 
         for reference, host_details in hosts.items():
             output.append('Host {}'.format(reference))
+
+            # if it's a string then it's a straight reference to IP or hostname mapping
+            if isinstance(host_details, str):
+                host_details = {
+                    'HostName': host_details
+                }
+
             host_settings = {**settings, **host_details}
 
             for k, v in host_settings.items():
                 output.append('    {} {}'.format(k, v))
 
-            output.append("\n")
+            output.append("")
 
     if global_values is not None:
         output.append('Host *')
@@ -40,7 +74,7 @@ def process_yaml(ssh_config_yaml):
 
 
 def extract_section(section_name, ssh_config, default_to = None):
-    if ssh_config[section_name]:
+    if section_name in ssh_config:
         default_to = ssh_config[section_name]
         del ssh_config[section_name]
 
