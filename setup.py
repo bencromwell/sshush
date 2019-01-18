bencromwell/sshush

commit 0344c33cbc33e72ca02d4621a79032ffc4d8c2c6

Fix for nested host options not being set. #3

sshush/sshush.py
@@ -77,8 +77,6 @@ def process_yaml(ssh_config_yaml):
                 host_details = {
                     'HostName': host_details
                 }
-            else:
-                host_details = {}
 
             host_settings = {**settings, **host_details}
 

commit 7cc35904f705eae1978aebf32bd02b43e1c8f01a

Fix regression caused by 0344c33
@todo - learn how to unit test properly in python!

setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='sshush',
-      version='1.3.1',
+      version='1.3.3',
       description='SSH Config management tool',
       url='http://github.com/bencromwell/sshush',
       author='Ben Cromwell',

sshush/sshush.py
@@ -73,7 +73,9 @@ def process_yaml(ssh_config_yaml):
             output.append('Host {}'.format(reference))
 
             # if it's a string then it's a straight reference to IP or hostname mapping
-            if isinstance(host_details, str) and not host_details.__contains__('*'):
+            if host_details.__contains__('*'):
+                host_details = {}
+            elif isinstance(host_details, str):
                 host_details = {
                     'HostName': host_details
                 }